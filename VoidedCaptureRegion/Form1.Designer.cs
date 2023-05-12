363197df64d1add98200069465e4ee3319222089
Kulpot <kulpotwahu@gmail.com>
2023-05-12 21:07:42 +0800

    Add project files.

---
+++ VoidedCaptureRegion/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace VoidedCaptureRegion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnVoid = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1126, 481);
+            this.panel.TabIndex = 0;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCapture.Location = new System.Drawing.Point(1047, 495);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(91, 23);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // btnVoid
+            // 
+            this.btnVoid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnVoid.Location = new System.Drawing.Point(950, 495);
+            this.btnVoid.Name = "btnVoid";
+            this.btnVoid.Size = new System.Drawing.Size(91, 23);
+            this.btnVoid.TabIndex = 2;
+            this.btnVoid.Text = "Void";
+            this.btnVoid.UseVisualStyleBackColor = true;
+            this.btnVoid.Click += new System.EventHandler(this.btnVoid_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1150, 521);
+            this.Controls.Add(this.btnVoid);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Button btnVoid;
+    }
+}
+
---
+++ VoidedCaptureRegion/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Diagnostics;
+
+namespace VoidedCaptureRegion
+{
+    public partial class Form1 : Form
+    {
+        private Point _lastPos;
+        private bool _isVoid;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            _lastPos = e.Location;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if(e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this.Top += e.Y - _lastPos.Y;
+                this.Left += e.X - _lastPos.X;
+            }
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if(e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnVoid_Click(object sender, EventArgs e)
+        {
+            using(GraphicsPath GP = new GraphicsPath())
+            {
+                if (_isVoid)
+                {
+                    GP.AddRectangle(this.ClientRectangle);
+                    _isVoid = false;
+                }
+                else
+                {
+                    GP.AddRectangle(this.ClientRectangle);
+                    GP.AddRectangle(panel.Bounds);
+                    _isVoid = true;
+                }
+                
+                this.Region = new Region(GP);
+            }
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            using (Bitmap bitmap = new Bitmap(panel.Width, panel.Height))
+            { 
+                using (Graphics GFX = Graphics.FromImage(bitmap))
+                {
+                    GFX.CopyFromScreen(this.PointToScreen(panel.Location), Point.Empty, panel.Size);
+                }
+
+                bitmap.Save("hey.bmp");
+                Process.Start("hey.bmp");
+            }
+        }
+    }
+}
